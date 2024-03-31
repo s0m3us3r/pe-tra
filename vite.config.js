0364c84639949ec649356fb8cb0ad28a9dad0460
s0m3us3r <132075491+s0m3us3r@users.noreply.github.com>
2024-03-31 15:17:43 +0300

    viilausta

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/frontend-petra/',
+  base: '/pe-tra/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
